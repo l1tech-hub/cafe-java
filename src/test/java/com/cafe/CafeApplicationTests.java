2c3b3b1a125d6129085d20e7dcc72ead2ae0b8d9
l1tech-hub <l1tech@proton.me>
2026-03-02 16:00:49 +0300

    sonarclaude 0

--- src/test/java/com/cafe/CafeApplicationTests.java
+++ src/test/java/com/cafe/CafeApplicationTests.java
@@ -6,8 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CafeApplicationTests {
 
+
   @Test
   void contextLoads() {
+    //Этот метод пуст, так как в программе не используются тесты.
   }
 
 }
