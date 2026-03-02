07a19ebf14561d16fc626bbb398e2258c70139e1
l1tech-hub <l1tech@proton.me>
2026-03-02 16:00:49 +0300

    sonarqube 0

--- src/test/java/com/cafe/CafeApplicationTests.java
+++ src/test/java/com/cafe/CafeApplicationTests.java
@@ -6,6 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CafeApplicationTests {
 
+  /**
+   * Этот мед пуст, так как в программе не используются тесты.
+   */
   @Test
   void contextLoads() {
   }
