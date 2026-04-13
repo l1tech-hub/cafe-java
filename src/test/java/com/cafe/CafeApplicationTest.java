88694c14486825e48581e12aeae210618360394f
l1tech-hub <l1tech@proton.me>
2026-04-13 09:51:19 +0300

    unit-tests for BatchService and DishService added

---
+++ src/test/java/com/cafe/CafeApplicationTest.java
@@ -0,0 +1,12 @@
+package com.cafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
---
+++ src/test/java/com/cafe/service/BatchServiceTest.java
@@ -0,0 +1,4 @@
+package com.cafe.service;
+
+public class BatchServiceTest {
+}
