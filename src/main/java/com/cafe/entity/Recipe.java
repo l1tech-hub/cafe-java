66ccf4fce3f1702ed1ae7dff5a65700385b125a2
l1tech-hub <l1tech@proton.me>
2026-05-23 17:31:17 +0300

    fixed class

--- src/main/java/com/cafe/entity/Recipe.java
+++ src/main/java/com/cafe/entity/Recipe.java
@@ -1,6 +1,7 @@
 package com.cafe.entity;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
